1f0c7b40a4f5cd036b888e6fefd81ac649f6066e
psycho-70 <furqankhan503503@gmail.com>
2024-08-15 04:26:22 -0700

    fix bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the chunk size limit to 1000 kB
+  },
 })
