6a9ab4c3df41a88ad6dfb36e9989798454f60ea7
psycho-70 <furqankhan503503@gmail.com>
2024-08-21 11:02:40 -0700

    error in blog

--- frontend/src/components/blog.jsx
+++ frontend/src/components/blog.jsx
@@ -13,6 +13,7 @@ const Blog = () => {
         const apiKey = import.meta.env.VITE_NEWSAPI_KEY;
         const res = await fetch(`https://newsapi.org/v2/everything?q=technology&apiKey=${apiKey}`);
         console.log('Response status:', res.status);
+        console.log('Response headers:', res.headers);
         if (!res.ok) {
           throw new Error(`Network response was not ok: ${res.statusText}`);
         }
@@ -30,6 +31,7 @@ const Blog = () => {
     fetchData();
   }, []);
   
+
   const backgroundImage = getBackgroundImage();
 
   return (
