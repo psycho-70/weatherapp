2eeab3fa5fca3c45ae2cabd1394341bc779c07e6
psycho-70 <furqankhan503503@gmail.com>
2024-08-23 05:03:31 -0700

    change the houly file

--- frontend/src/components/hourly.jsx
+++ frontend/src/components/hourly.jsx
@@ -5,14 +5,14 @@ const Hourly = () => {
   const { hourlyData, weatherData, darkMode, getBackgroundImage } = useContext(AppContext);
 
   return (
-    <div className={`flex min-h-screen flex-col items-center p-4 ${getBackgroundImage()} ${darkMode ? 'dark' : ''}`}>
+    <div className={`flex h-full flex-col items-center ${getBackgroundImage()} ${darkMode ? 'dark' : ''}`}>
       <h1 className="text-2xl text-white font-bold mb-4">Hourly Weather Data</h1>
       {hourlyData.length > 0 ? (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+        <div className=" justify-center flex flex-wrap gap-3 py-5 md:gap-10 items-center">
           {hourlyData.map((hour, index) => (
             <div
               key={index}
-              className="bg-white bg-opacity-30 text-white rounded-full shadow-md p-4 flex flex-col items-center justify-center w-40 h-40"
+              className="bg-black bg-opacity-30 text-white rounded-full shadow-md p-4 flex flex-col items-center justify-center w-40 h-40"
             >
               <p className="text-sm font-semibold">
                 {new Date(hour.dt * 1000).toLocaleTimeString()}
