996607d9f6a097242ab0bcee58b8b38dc58df5e7
psycho-70 <furqankhan503503@gmail.com>
2024-09-17 02:31:37 -0700

    add env

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,14 +3,17 @@ import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, signO
 import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, query, where } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6KViYWAZe1sBqaRrCS8qVwzoXfvE2Q2w",
-  authDomain: "first-try-a4dff.firebaseapp.com",
-  projectId: "first-try-a4dff",
-  storageBucket: "first-try-a4dff.appspot.com",
-  messagingSenderId: "816921935684",
-  appId: "1:816921935684:web:7782cef3e365a572532111",
-  measurementId: "G-9L1J2JPX7K"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
